e908b0cb8c58bb17b2d8d683fe2c71b85fe21d35
Yashwadkar2121 <yashwadkar143@gmail.com>
2024-01-20 03:41:32 +0530

    Add Some Other Functions

--- src/Components/02-TextForm.js
+++ src/Components/02-TextForm.js
@@ -23,6 +23,26 @@ function TextForm(props) {
     let newText = "";
     setText(newText);
   };
+  //   To copy Text
+  const handleCopyClick = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  //   Remove Extra Spaces
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  //   Capitalization
+  const handleCapitalization = () => {
+    let newText = text.split(" ");
+    for (let i = 0; i < newText.length; i++) {
+      newText[i] = newText[i].charAt(0).toUpperCase() + newText[i].slice(1);
+    }
+    let newText2 = newText.join(" ");
+    setText(newText2);
+  };
   return (
     <>
       {/* Creating a textarea */}
@@ -46,8 +66,26 @@ function TextForm(props) {
         <button className="btn btn-primary mx-2 my-1" onClick={handleLowClick}>
           Convert To Lowercase
         </button>
-        <button className="btn btn-danger mx-2 my-1" onClick={handleClearClick}>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleClearClick}
+        >
           Clear
+        </button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleCapitalization}
+        >
+          Capitalization
         </button>
       </div>
       {/* Text Summary section */}
